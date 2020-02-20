286d6d3cfe4c489c4bd559d546e77952ddedf8c2
Florian Geckeler <f.geckeler@sap.com>
2020-02-20 13:29:38 +0100

    further wip

--- cmd/mavenExecute.go
+++ cmd/mavenExecute.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"bytes"
 	"io"
-	"os"
 
 	"github.com/SAP/jenkins-library/pkg/command"
 	"github.com/SAP/jenkins-library/pkg/log"
@@ -12,74 +11,75 @@ import (
 
 const mavenExecutable = "mvn"
 
-func mavenExecute(config mavenExecuteOptions, telemetryData *telemetry.CustomData) {
-	c := command.Command{}
-	// stdOud := log.Entry().Writer()
+func mavenExecute(config mavenExecuteOptions, telemetryData *telemetry.CustomData) string {
+	var stdOut io.Writer
+	var stdOutBuf *bytes.Buffer
 
-	// if config.ReturnStdout {
-	// 	stdOutBuf = new(bytes.Buffer)
-	// 	stdOut = io.MultiWriter(log.Entry().Writer(), &stdOutBuf)
-	// }
-	stdOutBuf := new(bytes.Buffer)
-	stdOut := io.MultiWriter(os.Stdout, stdOutBuf)
+	c := command.Command{}
+	stdOut = log.Entry().Writer()
+
+	if config.ReturnStdout {
+		stdOutBuf = new(bytes.Buffer)
+		stdOut = io.MultiWriter(log.Entry().Writer(), stdOutBuf)
+	}
 
 	c.Stdout(stdOut)
 	c.Stderr(log.Entry().Writer())
 
-	// error situations should stop execution through log.Entry().Fatal() call which leads to an os.Exit(1) in the end
-	err := runMavenExecute(config, &c) //runMavenExecute(&config, telemetryData, &c)
+	err := runMavenExecute(&config, &c)
 	if err != nil {
 		log.Entry().WithError(err).Fatal("step execution failed")
 	}
+
+	if stdOutBuf == nil {
+		return ""
+	}
+	return string(stdOutBuf.Bytes())
 }
 
-func runMavenExecute(config mavenExecuteOptions, command execRunner) error {
-	return command.RunExecutable("mvn", "--version")
+func runMavenExecute(config *mavenExecuteOptions, command execRunner) error {
+
+	parameters := []string{}
+
+	if config.GlobalSettingsFile != "" {
+		globalSettingsFileParameter := "--global-settings " + config.GlobalSettingsFile
+		parameters = append(parameters, globalSettingsFileParameter)
+	}
+
+	if config.ProjectSettingsFile != "" {
+		projectSettingsFileParameter := "--settings " + config.ProjectSettingsFile
+		parameters = append(parameters, projectSettingsFileParameter)
+	}
+
+	if config.M2Path != "" {
+		m2PathParameter := "-Dmaven.repo.local=" + config.M2Path
+		parameters = append(parameters, m2PathParameter)
+	}
+
+	if config.PomPath != "" {
+		pomPathParameter := "--file " + config.PomPath
+		parameters = append(parameters, pomPathParameter)
+	}
+
+	if config.Flags != nil {
+		parameters = append(parameters, config.Flags...)
+	}
+
+	parameters = append(parameters, "--batch-mode")
+
+	if config.LogSuccessfulMavenTransfers {
+		parameters = append(parameters, "-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn")
+	}
+
+	parameters = append(parameters, config.Goals...)
+
+	err := command.RunExecutable(mavenExecutable, parameters...)
+	if err != nil {
+		log.Entry().
+			WithError(err).
+			WithField("command", append([]string{mavenExecutable}, parameters...)).
+			Fatal("failed to execute run command")
+	}
+
+	return nil
 }
-
-// func runMavenExecute(config *mavenExecuteOptions, telemetryData *telemetry.CustomData, command execRunner) error, string {
-
-// 	parameters := []string{}
-
-// 	if config.GlobalSettingsFile != "" {
-// 		globalSettingsFileParameter := "--global-settings " + config.GlobalSettingsFile
-// 		parameters = append(parameters, globalSettingsFileParameter)
-// 	}
-
-// 	if config.ProjectSettingsFile != "" {
-// 		projectSettingsFileParameter := "--settings " + config.ProjectSettingsFile
-// 		parameters = append(parameters, projectSettingsFileParameter)
-// 	}
-
-// 	if config.M2Path != "" {
-// 		m2PathParameter := "-Dmaven.repo.local=" + config.M2Path
-// 		parameters = append(parameters, m2PathParameter)
-// 	}
-
-// 	if config.PomPath != "" {
-// 		pomPathParameter := "--file " + config.PomPath
-// 		parameters = append(parameters, pomPathParameter)
-// 	}
-
-// 	if config.Flags != nil {
-// 		parameters = append(parameters, config.Flags...)
-// 	}
-
-// 	parameters = append(parameters, "--batch-mode")
-
-// 	if config.LogSuccessfulMavenTransfers {
-// 		parameters = append(parameters, "-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn")
-// 	}
-
-// 	parameters = append(parameters, config.Goals...)
-
-// 	err := command.RunExecutable(mavenExecutable, parameters...)
-// 	if err != nil {
-// 		log.Entry().
-// 			WithError(err).
-// 			WithField("command", config.RunCommand).
-// 			Fatal("failed to execute run command")
-// 	}
-
-// 	return nil
-// }
--- cmd/mavenExecute_test.go
+++ cmd/mavenExecute_test.go
@@ -10,25 +10,25 @@ import (
 	"github.com/SAP/jenkins-library/pkg/command"
 )
 
- type myWriter struct (
-	func Write(p []byte)  (n int, err error) {
-		return
-	}
-)
-
 func TestMavenExecute(t *testing.T) {
 	t.Run("maven version", func(t *testing.T) {
 		opts := mavenExecuteOptions{}
 		c := command.Command{}
 		stdOutBuf := new(bytes.Buffer)
-		stdOut := io.MultiWriter(os.Stderr, stdOutBuf)
-		c.Stdout(stdOut)
-		t.Log("test")
-		err := runMavenExecute(opts, &c)
+
+		outfile, err := os.Create("test.txt")
 		if err != nil {
-			fmt.Println("err")
+			fmt.Printf("error: %v", err)
 		}
-		t.Logf ("my buffer: %v",string(stdOutBuf.Bytes()))
+		defer outfile.Close()
+
+		stdOut := io.MultiWriter(outfile, stdOutBuf)
+		c.Stdout(stdOut)
+		err = runMavenExecute(&opts, &c)
+		if err != nil {
+			fmt.Printf("error: %v", err)
+		}
+		t.Logf("my buffer: %v", string(stdOutBuf.Bytes()))
 
 	})
 }
